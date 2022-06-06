1cc6100de6784147e7a96b1600190fa7b879d8b2
Justin Blau <justin.blau8@gmail.com>
2022-06-06 03:47:05 +0000

    update listener script

--- demo/src/main/resources/consumer-pull.sh
+++ demo/src/main/resources/consumer-pull.sh
@@ -1,4 +1,6 @@
 sudo pkill java
+rm -r /home/opc/SQL
+scp -r 141.147.102.85:/home/opc/f1-game-listener/demo/src/main/java/Repository/SQL .
 scp -r 10.0.0.125:/home/opc/f1-game-listener/demo/target .
 source envs
 nohup java -jar target/demo-1.jar &
